d982d186a3936b5fa449c11d0e18b35c27d9e2dd
Vlad <vladosmen29@gmail.com>
2024-10-14 14:17:52 +0600

    update payment history

--- src/localpay/serializers/payment_serializers/payment_history_serializer.py
+++ src/localpay/serializers/payment_serializers/payment_history_serializer.py
@@ -4,7 +4,12 @@ from rest_framework.pagination import PageNumberPagination
 
 
 class PaymentHistorySerializer(serializers.ModelSerializer):
+    user_name = serializers.SerializerMethodField()  
+
     class Meta:
         model = Pays
-        fields = ['number_payment', 'date_payment', 'accept_payment', 'ls_abon', 'money', 'status_payment']
+        fields = ['number_payment', 'date_payment', 'accept_payment', 'ls_abon', 'money', 'status_payment' , 'user_name']
+
+    def get_user_name(self, obj):
+        return f"{obj.user.name} {obj.user.surname}"
 
--- src/localpay/views/payment_views/payment_history.py
+++ src/localpay/views/payment_views/payment_history.py
@@ -9,7 +9,9 @@ from localpay.serializers.user import UserSerializer , PaysSerializer
 from rest_framework.pagination import PageNumberPagination
 from localpay.serializers.payment_serializers.payment_history_serializer import PaymentHistorySerializer
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-
+from drf_yasg.utils import swagger_auto_schema
+from localpay.schema.swagger_schema import search_param
+from django.db.models import Q
 
 
 
@@ -18,33 +20,29 @@ class PaymentHistoryListAPIView(ListAPIView):
     permission_classes = [IsAdmin | IsSupervisor]
     serializer_class = PaymentHistorySerializer
 
+    @swagger_auto_schema(manual_parameters=[search_param])
+    def list(self, request, *args, **kwargs):
+        search_query = request.query_params.get('search', '')
+
+        if search_query:
+            queryset = Pays.objects.filter(
+                Q(ls_abon__icontains=search_query) |  
+                Q(user__login__icontains=search_query) 
+            )
+        else:
+            queryset = Pays.objects.all()
+
+        total_count = queryset.count()
+
+        page_size = int(request.query_params.get('page_size', 50))
+        page_number = request.GET.get('page', 1)
+        paginator = PageNumberPagination()
+        paginator.page_size = page_size
+        paginated_queryset = paginator.paginate_queryset(queryset, request)
+
+        serializer = self.get_serializer(paginated_queryset, many=True)
+
+        return paginator.get_paginated_response(serializer.data)
 
     def get_queryset(self):
         return Pays.objects.all()
-
-    def get(self, request, *args, **kwargs):
-        payments = Pays.objects.all()
-
-        total_payments = payments.count()
-
-        page_size = int(request.query_params.get('page_size', 50)) 
-        page_number = request.GET.get('page', 1)
-        paginator = Paginator(payments, page_size) 
-
-        try:
-            payment = paginator.page(page_number)  
-        except PageNotAnInteger:
-
-            payment = paginator.page(1)
-        except EmptyPage:
-            payment = paginator.page(paginator.num_pages)
-
-        serializer = self.get_serializer(payment, many=True)
-
-        return Response({
-            'count':total_payments,
-            'total_pages': paginator.num_pages,
-            'page_size': page_size,
-            'current_page': page_number, 
-            'results': serializer.data,
-            }, status=status.HTTP_200_OK)
