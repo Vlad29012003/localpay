36c2e6e5e3be051cbf3da977bf36fe120a020814
Vlad <vladosmen29@gmail.com>
2024-10-16 14:48:58 +0600

    aded a loging in permisson

--- src/localpay/permission.py
+++ src/localpay/permission.py
@@ -5,24 +5,31 @@ logger = logging.getLogger(__name__)
 logging.basicConfig(
     filename='permissions.log',
     level=logging.INFO,
-    format= '%(asctime)s - %(levelname)'
+    format= '%(asctime)s - %(levelname)s - %(message)s'
 )
+
+logger = logging.getLogger(__name__)
 
 # responsible role for base user
 class IsUser(permissions.BasePermission):
     def has_permission(self, request, view):
-        return request.user.is_authenticated and request.user.role == 'user'
-        logger.info(f'User {request.user.username}')
+        is_authenticated = request.user.is_authenticated and request.user.role == 'user'
+        logger.info(f'User {request.user.username} accessed {view.__class__.__name__} with permission {is_authenticated}')
+
+        return is_authenticated
     
 
 # responsible role for supervisor
 class IsSupervisor(permissions.BasePermission):
     def has_permission(self, request, view):
-        return request.user.is_authenticated and request.user.role == 'supervisor'
-        logger.info(f'User {request.user.username}')
+        is_authenticated = request.user.is_authenticated and request.user.role == 'supervisor'
+        logger.info(f'Supervisor {request.user.username} accessed {view.__class__.__name__} with permission {is_authenticated}')
+        return is_authenticated
     
 
 # responsible role for admin
 class IsAdmin(permissions.BasePermission):
     def has_permission(self, request, view):
-        return request.user.is_authenticated and request.user.role == 'admin'
+        is_authenticated = request.user.is_authenticated and request.user.role == 'supervisor'
+        logger.info(f'Admin {request.user.username} accessed {view.__class__.__name__} with permission {is_authenticated}')
+        return is_authenticated
