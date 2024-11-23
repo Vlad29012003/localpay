5caa6ec5425a2255be928fade9a106542c7eba2c
Vlad <vladosmen29@gmail.com>
2024-11-23 12:10:42 +0600

    aded a test safe version

--- src/localpay/tests/pays/test_create_payment.py
+++ src/localpay/tests/pays/test_create_payment.py
@@ -1,11 +1,9 @@
 import pytest
 from rest_framework import status
-from localpay.models import User_mon , Pays
+from localpay.models import User_mon
 from rest_framework.test import APIClient
 
 
-
-# Аунтефикация пользователя
 @pytest.fixture
 def authenticated_user_client(db):
     admin_user = User_mon.objects.create_user(
@@ -18,13 +16,12 @@ def authenticated_user_client(db):
         balance=1000.0,
         role="user"
     )
-    
     client = APIClient()
     client.force_authenticate(user=admin_user)
     
     return client
 
-# Создаем пользователя 
+
 @pytest.fixture
 def create_user():
     def _create_user(login, password, name, surname, avail_balance, region, balance, role):
@@ -41,32 +38,60 @@ def create_user():
     return _create_user
 
 
+# Проверка успешного создания Платежа
 @pytest.mark.django_db
 def test_create_payment_success(authenticated_user_client):
-    # Убедитесь, что пользователь имеет достаточно средств и доступ
     user = User_mon.objects.filter(login="user").first()
     if user:
-        user.balance = 1000.0  # Установите достаточный баланс
+        user.balance = 1000.0 
         user.avail_balance = 1000.0
-        user.access = True    # Обеспечьте доступ
+        user.access = True 
         user.save()
 
     # Данные платежа
     payment_data = {
         "ls": 175050620,
-        "login": "user",  # Логин аутентифицированного пользователя
+        "login": "user",
         "money": 50.0,
         "service_type": "test_service",
         "comment": "Test payment",
     }
 
-    # Отправляем запрос на создание платежа
     response = authenticated_user_client.post("/api/create-payment/", payment_data)
 
-    # Проверяем, что запрос был успешным
     assert response.status_code == status.HTTP_200_OK
     assert "transaction_id" in response.data
     assert "sum" in response.data
     assert "status" in response.data
     assert response.data["status"] == "0"
+
+
+
+# Проверка платежа с недостаточным балансом
+@pytest.mark.django_db
+def test_create_payment_insufficient_balance(authenticated_client, create_user):
+    user = create_user(
+        login="testuser",
+        password="password123",
+        name="Test",
+        surname="User",
+        avail_balance=20.0,
+        region="Чуйская",
+        balance=30.0,
+        role="user",
+    )
+
+    payment_data = {
+        "ls": 12345,
+        "login": "testuser",
+        "money": 50.0,
+        "service_type": "test_service",
+        "comment": "Test payment",
+    }
+
+    response = authenticated_client.post("/api/create-payment/", payment_data)
+
+    # Проверяем, код ошибки
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+    assert response.data["detail"] == "You do not have permission to perform this action."
 
